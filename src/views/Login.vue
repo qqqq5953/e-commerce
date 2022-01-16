06d77a53d81b1063fbd2af4c024dd3dfbbc4d4dd
andy <qqqq5953@gmail.com>
2022-01-16 12:54:43 +0800

    login error

---
+++ src/views/Login.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container mt-5">
+    <form class="row justify-content-center" @submit.prevent="signIn">
+      <div class="col-md-10 col-lg-6">
+        <h1 class="h3 mb-3 font-weight-normal">請先登入</h1>
+        <div class="mb-3">
+          <label for="exampleInputEmail1" class="form-label"
+            >Email address</label
+          >
+          <input
+            type="email"
+            class="form-control"
+            id="exampleInputEmail1"
+            placeholder="Email address"
+            aria-describedby="emailHelp"
+            v-model="user.username"
+            autofocus
+            required
+          />
+          <div id="emailHelp" class="form-text">
+            We'll never share your email with anyone else.
+          </div>
+        </div>
+        <div class="mb-3">
+          <label for="exampleInputPassword1" class="form-label">Password</label>
+          <input
+            type="password"
+            class="form-control"
+            placeholder="Password"
+            id="exampleInputPassword1"
+            v-model="user.password"
+            required
+          />
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        username: '',
+        password: ''
+      }
+    };
+  },
+  methods: {
+    signIn() {
+      const api = `${process.env.VUE_APP_API}admin/signin`;
+      console.log('api', api);
+      this.$http.post(api, this.user).then((res) => console.log(res));
+    }
+  }
+};
+</script>
